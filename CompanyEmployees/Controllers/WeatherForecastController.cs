71be1936e5de3383a4cb984b939c05667d1ba1ce
Анастасия Волкова <anastsiavolkova@gmail.com>
2025-04-08 21:36:52 +0300

    11

--- CompanyEmployees/Controllers/WeatherForecastController.cs
+++ CompanyEmployees/Controllers/WeatherForecastController.cs
@@ -1,32 +1,28 @@
+using Contracts;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CompanyEmployees.Controllers;
 
-[ApiController]
 [Route("[controller]")]
+[ApiController]
 public class WeatherForecastController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
+    private ILoggerManager _logger;
+    public WeatherForecastController(ILoggerManager logger)
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
 
-    private readonly ILogger<WeatherForecastController> _logger;
-
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
-    {
         _logger = logger;
     }
-
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    [HttpGet]
+    public IEnumerable<string> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        _logger.LogInfo("¬от информационное сообщение от нашего контроллеразначений.");
+
+        _logger.LogDebug("¬от отладочное сообщение от нашего контроллеразначений.");
+
+        _logger.LogWarn("¬от сообщение предупреждени€ от нашего контроллера значений.");
+
+        _logger.LogError("¬от сообщение об ошибке от нашего контроллеразначений.");
+        return new string[] { "value1", "value2" };
     }
 }
